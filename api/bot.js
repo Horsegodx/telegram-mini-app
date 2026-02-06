0efd4f62dd3ad35d3b5542f8772976a6d8c0bf04
Horsegodx <serge200516@gmail.com>
2026-02-06 19:29:13 +0300

    6.4.4.4.4

---
+++ api/bot.js
@@ -0,0 +1,47 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(200).send("OK");
+  }
+
+  const TELEGRAM_TOKEN = "8576035098:AAHeIupZSQsZLD0aUUBsBvTZAxlsCqdf8aM";
+  const WEB_APP_URL = "https://telegram-mini-app-nu-jet.vercel.app";
+
+  const body = req.body;
+  const chatId = body.message?.chat?.id;
+  const text = body.message?.text;
+
+  if (!chatId) {
+    return res.status(200).end();
+  }
+
+  // /start
+  if (text === "/start") {
+    const message = `Добрый день 👋
+
+Вы зашли в бота, где можно посмотреть помещения
+от Евгения Иванова 🏢
+
+Нажмите кнопку ниже, чтобы открыть каталог ⬇️`;
+
+    await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: message,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "📂 Открыть каталог",
+                web_app: { url: WEB_APP_URL }
+              }
+            ]
+          ]
+        }
+      })
+    });
+  }
+
+  res.status(200).end();
+}
